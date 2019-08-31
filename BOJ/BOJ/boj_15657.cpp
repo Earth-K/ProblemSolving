1edc6875f5da54b0f763ff1755d7aafb03739ccf
JIGU KWAK <k94earth@naver.com>
2019-08-31 23:00:03 +0900

    N과 M(8) (DFS)

---
+++ BOJ/BOJ/boj_15657.cpp
@@ -0,0 +1,35 @@
+#include<cstdio>
+#include<algorithm>
+/*
+	2019. 08. 31.
+	[BOJ] น้มุ 15657. Nฐ๚ M (8)
+
+	DFS
+*/
+int arr[8], ans[8];
+int N, M;
+
+void perm(int idx, int pre) {
+	if (idx == M) {
+		for (int i = 0; i<M; i++)
+			printf("%d ", ans[i]);
+		printf("\n");
+		return ;
+	}
+
+	for (int i = 0; i<N; i++) {
+		if (pre <= arr[i]) {
+			ans[idx] = arr[i];
+			perm(idx+1, arr[i]);
+		}
+	}
+}
+
+int main() {
+	scanf("%d%d", &N, &M);
+	for (int i = 0; i<N; i++)
+		scanf("%d", &arr[i]);
+	std::sort(arr,arr+N);
+	perm(0, arr[0]);
+	return 0;
+}
